4a568cb41f9466fdd2afe959cd8490197a93604b
Philip Ricciuti <PhilipRicciuti@Outlook.com>
2025-09-03 18:26:02 -0400

    9/3/2025

---
+++ scripts/sandbox.js
@@ -0,0 +1,20 @@
+let name = 'Hello World';
+
+console.log(name);
+
+let person = {
+    name: 'John',
+    age: 25
+}
+
+person.name = 'Jane';
+person['name'] = 'John';
+
+// Variables cannot be a reserved keyword
+// Let declares a variable
+// Cannot start with a number
+// Cannot contain a space or hyphen
+// Let can let you reassign, const will not let you reassign
+// typeof declares the type of a variable
+// All numbers are of type number, there are no ints and floats
+//
