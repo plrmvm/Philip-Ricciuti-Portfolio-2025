d577a671aaf21600cee4d5edf13c0d917812e182
plrmvm <134121188+plrmvm@users.noreply.github.com>
2025-09-04 21:07:58 -0400

    9/4/2025 12:50pm

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.querySelector('.hamburger-button');
-    const sidebar = document.querySelector('.sidebar');
+    const menu = document.querySelector('.mobile-nav-menu');
     const main_content = document.querySelector('.main-content');
     let clicked = false;
     button.addEventListener("click", () => {
         console.log("clicked")
         if (clicked == false) {
-            sidebar.style.visibility = "visible";
-            sidebar.style.opacity = "1";
-            main_content.style.opacity = "0";
+            menu.zIndex = "8";
+            menu.style.visibility = "visible";
+            menu.style.opacity = "1";
             clicked = true;
         }
         else {
-            main_content.style.opacity = "1";
-            sidebar.style.opacity = "0";
-            sidebar.style.visibility = "hidden";
+            menu.style.opacity = "0";
+            menu.style.visibility = "hidden";
+            menu.zIndex = "-1";
             clicked = false;
         }
 
