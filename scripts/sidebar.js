4505a319053714845d8c6ce8f20154716a5bf81e
Louan Flammanc <kusami.louan@gmail.com>
2025-09-06 14:32:51 -0400

    sidebar is now dynamically added to each page

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,4 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
+    if (!document.querySelector('.sidebar')) {
+        const sidebar = document.createElement('div');
+        sidebar.className = 'sidebar';
+        sidebar.innerHTML = `
+            <div class="sidebar-logo">
+                <a href="index.html" class="nameblock">PHILIP <br>RICCIUTI<br></a>
+                <div>
+                    <a href="index.html" class="nameblock-subtext">3D ARTIST & DESIGNER</a>
+                </div>
+            </div>
+            <div class="sidebar-nav">
+                <ul>
+                    <li class="sidebar-item">
+                        <a href="index.html" class="sidebar-text${window.location.pathname.endsWith('index.html') ? ' active' : ''}">
+                            <span class="nav-dot${window.location.pathname.endsWith('index.html') ? ' active' : ''}"></span>
+                            3D Design
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="photography.html" class="sidebar-text${window.location.pathname.endsWith('photography.html') ? ' active' : ''}">
+                            <span class="nav-dot${window.location.pathname.endsWith('photography.html') ? ' active' : ''}"></span>
+                            Photography
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="about.html" class="sidebar-text${window.location.pathname.endsWith('about.html') ? ' active' : ''}">
+                            <span class="nav-dot${window.location.pathname.endsWith('about.html') ? ' active' : ''}"></span>
+                            About
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="contact.html" class="sidebar-text${window.location.pathname.endsWith('contact.html') ? ' active' : ''}">
+                            <span class="nav-dot${window.location.pathname.endsWith('contact.html') ? ' active' : ''}"></span>
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="sidebar-blank">
+                <footer class="sidebar-email">PhilipRicciuti@outlook.com</footer>
+            </div>
+            <div class="sidebar-line"></div>
+        `;
+        document.body.insertBefore(sidebar, document.body.firstChild);
+    }
     const button = document.querySelector('.hamburger-button');
     const sidebar = document.querySelector('.sidebar');
     const main_content = document.querySelector('.main-content');
